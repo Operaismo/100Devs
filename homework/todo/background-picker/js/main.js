69e743ef9eda2625a627bb690f280a46fea1e397
Operaismo <eloperaismo@protonmail.com>
2022-10-05 17:46:49 -0500

    feat: added moveset on click event handler

--- homework/todo/background-picker/js/main.js
+++ homework/todo/background-picker/js/main.js
@@ -8,6 +8,7 @@ document.getElementById('whimsicott').onclick = switchWhimsicott;
 function switchZacian() {
   document.querySelector('body').style.backgroundColor = 'rgba(63, 180, 247)';
   document.querySelector('body').style.color = 'white';
+  // Switch Image On Click
   document.getElementById('pokeball').hidden = true;
   document.getElementById('zacian-img').hidden = false;
   document.getElementById('groudon-img').hidden = true;
@@ -15,11 +16,20 @@ function switchZacian() {
   document.getElementById('charizard-img').hidden = true;
   document.getElementById('lunala-img').hidden = true;
   document.getElementById('whimsicott-img').hidden = true;
+  // Switch Moveset On Click
+  document.getElementsByClassName('moveset')[0].hidden = true;
+  document.getElementsByClassName('moveset-zacian')[0].hidden = false;
+  document.getElementsByClassName('moveset-groudon')[0].hidden = true;
+  document.getElementsByClassName('moveset-kyogre')[0].hidden = true;
+  document.getElementsByClassName('moveset-charizard')[0].hidden = true;
+  document.getElementsByClassName('moveset-lunala')[0].hidden = true;
+  document.getElementsByClassName('moveset-whimsicott')[0].hidden = true;
 }
 
 function switchGroudon() {
   document.querySelector('body').style.backgroundColor = 'rgba(253, 34, 0)';
   document.querySelector('body').style.color = 'white';
+  // Switch Image On Click
   document.getElementById('pokeball').hidden = true;
   document.getElementById('zacian-img').hidden = true;
   document.getElementById('groudon-img').hidden = false;
@@ -27,11 +37,20 @@ function switchGroudon() {
   document.getElementById('charizard-img').hidden = true;
   document.getElementById('lunala-img').hidden = true;
   document.getElementById('whimsicott-img').hidden = true;
+  // Switch Moveset On Click
+  document.getElementsByClassName('moveset')[0].hidden = true;
+  document.getElementsByClassName('moveset-zacian')[0].hidden = true;
+  document.getElementsByClassName('moveset-groudon')[0].hidden = false;
+  document.getElementsByClassName('moveset-kyogre')[0].hidden = true;
+  document.getElementsByClassName('moveset-charizard')[0].hidden = true;
+  document.getElementsByClassName('moveset-lunala')[0].hidden = true;
+  document.getElementsByClassName('moveset-whimsicott')[0].hidden = true;
 }
 
 function switchKyogre() {
   document.querySelector('body').style.backgroundColor = 'rgba(10, 46, 255)';
   document.querySelector('body').style.color = 'white';
+  // Switch Image On Click
   document.getElementById('pokeball').hidden = true;
   document.getElementById('zacian-img').hidden = true;
   document.getElementById('groudon-img').hidden = true;
@@ -39,11 +58,20 @@ function switchKyogre() {
   document.getElementById('charizard-img').hidden = true;
   document.getElementById('lunala-img').hidden = true;
   document.getElementById('whimsicott-img').hidden = true;
+  // Switch Moveset On Click
+  document.getElementsByClassName('moveset')[0].hidden = true;
+  document.getElementsByClassName('moveset-zacian')[0].hidden = true;
+  document.getElementsByClassName('moveset-groudon')[0].hidden = true;
+  document.getElementsByClassName('moveset-kyogre')[0].hidden = false;
+  document.getElementsByClassName('moveset-charizard')[0].hidden = true;
+  document.getElementsByClassName('moveset-lunala')[0].hidden = true;
+  document.getElementsByClassName('moveset-whimsicott')[0].hidden = true;
 }
 
 function switchCharizard() {
   document.querySelector('body').style.backgroundColor = 'rgba(255, 151, 25)';
   document.querySelector('body').style.color = 'white';
+  // Switch Image On Click
   document.getElementById('pokeball').hidden = true;
   document.getElementById('zacian-img').hidden = true;
   document.getElementById('groudon-img').hidden = true;
@@ -51,11 +79,20 @@ function switchCharizard() {
   document.getElementById('charizard-img').hidden = false;
   document.getElementById('lunala-img').hidden = true;
   document.getElementById('whimsicott-img').hidden = true;
+  // Switch Moveset On Click
+  document.getElementsByClassName('moveset')[0].hidden = true;
+  document.getElementsByClassName('moveset-zacian')[0].hidden = true;
+  document.getElementsByClassName('moveset-groudon')[0].hidden = true;
+  document.getElementsByClassName('moveset-kyogre')[0].hidden = true;
+  document.getElementsByClassName('moveset-charizard')[0].hidden = false;
+  document.getElementsByClassName('moveset-lunala')[0].hidden = true;
+  document.getElementsByClassName('moveset-whimsicott')[0].hidden = true;
 }
 
 function switchLunala() {
   document.querySelector('body').style.backgroundColor = 'rgba(113, 36, 207)';
   document.querySelector('body').style.color = 'white';
+  // Switch Image On Click
   document.getElementById('pokeball').hidden = true;
   document.getElementById('zacian-img').hidden = true;
   document.getElementById('groudon-img').hidden = true;
@@ -63,11 +100,20 @@ function switchLunala() {
   document.getElementById('charizard-img').hidden = true;
   document.getElementById('lunala-img').hidden = false;
   document.getElementById('whimsicott-img').hidden = true;
+  // Switch Moveset On Click
+  document.getElementsByClassName('moveset')[0].hidden = true;
+  document.getElementsByClassName('moveset-zacian')[0].hidden = true;
+  document.getElementsByClassName('moveset-groudon')[0].hidden = true;
+  document.getElementsByClassName('moveset-kyogre')[0].hidden = true;
+  document.getElementsByClassName('moveset-charizard')[0].hidden = true;
+  document.getElementsByClassName('moveset-lunala')[0].hidden = false;
+  document.getElementsByClassName('moveset-whimsicott')[0].hidden = true;
 }
 
 function switchWhimsicott() {
   document.querySelector('body').style.backgroundColor = 'rgba(31, 213, 74)';
   document.querySelector('body').style.color = 'white';
+  // Switch Image On Click
   document.getElementById('pokeball').hidden = true;
   document.getElementById('zacian-img').hidden = true;
   document.getElementById('groudon-img').hidden = true;
@@ -75,4 +121,12 @@ function switchWhimsicott() {
   document.getElementById('charizard-img').hidden = true;
   document.getElementById('lunala-img').hidden = true;
   document.getElementById('whimsicott-img').hidden = false;
+  // Switch Moveset On Click
+  document.getElementsByClassName('moveset')[0].hidden = true;
+  document.getElementsByClassName('moveset-zacian')[0].hidden = true;
+  document.getElementsByClassName('moveset-groudon')[0].hidden = true;
+  document.getElementsByClassName('moveset-kyogre')[0].hidden = true;
+  document.getElementsByClassName('moveset-charizard')[0].hidden = true;
+  document.getElementsByClassName('moveset-lunala')[0].hidden = true;
+  document.getElementsByClassName('moveset-whimsicott')[0].hidden = false;
 }
